23b47577818be6a2cb9b636377fc932aee6ce41e
hou27 <ataj125@gmail.com>
2022-07-20 02:07:05 +0900

    feat: 랜덤한 문자열 대신 UUID를 생성하는 방식으로 변경
    랜덤으로 생성하는 uuid v4를 사용함.

--- src/users/entities/verification.entity.ts
+++ src/users/entities/verification.entity.ts
@@ -2,6 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Column, Entity, JoinColumn, OneToOne, BeforeInsert } from 'typeorm';
 import { User } from './user.entity';
+import { v4 as uuidv4 } from 'uuid';
 
 @Entity()
 export class Verification extends CoreEntity {
@@ -15,6 +16,6 @@ export class Verification extends CoreEntity {
 
   @BeforeInsert()
   createCode(): void {
-    this.code = Math.random().toString(36).substring(2); //uuidv4();
+    this.code = uuidv4();
   }
 }

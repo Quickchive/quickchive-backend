00d5460cc1bd13e2de02e5925453129335f2bd8e
hou27 <ataj125@gmail.com>
2022-07-25 17:20:03 +0900

    feat: joi를 통한 환경변수 검증 및 cross-env를 통한 NODE_ENV 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,17 +11,39 @@ import { ContentsModule } from './contents/contents.module';
 import { Content } from './contents/entities/content.entity';
 import { Category } from './contents/entities/category.entity';
 import { DataSource } from 'typeorm';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
+        DB_HOST: Joi.string(),
+        DB_PORT: Joi.string(),
+        DB_USERNAME: Joi.string(),
+        DB_PW: Joi.string(),
+        DB_NAME: Joi.string(),
+        JWT_ACCESS_TOKEN_PRIVATE_KEY: Joi.string().required(),
+        JWT_REFRESH_TOKEN_PRIVATE_KEY: Joi.string().required(),
+        MAILGUN_API_KEY: Joi.string().required(),
+        MAILGUN_DOMAIN_NAME: Joi.string().required(),
+        MAILGUN_TEMPLATE_NAME: Joi.string().required(),
+      }),
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PW,
-      database: process.env.DB_NAME,
+      ...(process.env.DATABASE_URL
+        ? { url: process.env.DATABASE_URL }
+        : {
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PW,
+            database: process.env.DB_NAME,
+          }),
       synchronize: process.env.NODE_ENV !== 'prod',
       logging:
         process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',

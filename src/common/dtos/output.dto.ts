ead4176fc4687e912d30d573f1614d9a28497297
hou27 <ataj125@gmail.com>
2022-08-08 17:23:13 +0900

    improve: swagger api doc를 위한 property 추가
    CoreOutput에 statusCode property를 추가하여
    api 문서로 확인했을 때 interceptor 및 filter를 통해
    완성되는 response 구조를 알 수 있도록 함

--- src/common/dtos/output.dto.ts
+++ src/common/dtos/output.dto.ts
@@ -2,6 +2,12 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean } from 'class-validator';
 
 export class CoreOutput {
+  @ApiProperty({
+    description: 'Status Code',
+    example: 200,
+  })
+  statusCode?: number;
+
   @ApiProperty({ description: '에러 메시지', required: false })
   error?: string;
 }

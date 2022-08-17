2b0816299e8a630caf0bbd1f3b88c2872bfb00d5
hou27 <ataj125@gmail.com>
2022-08-17 19:12:27 +0900

    improve: 메일 인증 시 인증된 메일도 반환하도록 함

--- src/auth/dtos/verify-email.dto.ts
+++ src/auth/dtos/verify-email.dto.ts
@@ -1,3 +1,7 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { CoreOutput } from 'src/common/dtos/output.dto';
 
-export class VerifyEmailOutput extends CoreOutput {}
+export class VerifyEmailOutput extends CoreOutput {
+  @ApiProperty({ description: '인증된 이메일', required: false })
+  email?: string;
+}

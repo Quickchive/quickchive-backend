afb67ec3b0f8620a7d9db60fe0438a664117b86f
hou27 <ataj125@gmail.com>
2022-09-11 20:29:18 +0900

    feat: content의 deadline을 확인하는 로직 작성
    timezone 문제로 의도한 결과가 나오지 않는 경우를
    방지하기 위해 UTC 표준시로 현재 날짜 객체를 생성하여
    탐색하도록 함

--- src/batch/batch.module.ts
+++ src/batch/batch.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Content } from 'src/contents/entities/content.entity';
 import { TaskService } from './task.service';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [ScheduleModule.forRoot(), TypeOrmModule.forFeature([Content])],
   providers: [TaskService],
 })
 export class BatchModule {}
--- src/batch/task.service.ts
+++ src/batch/task.service.ts
@@ -1,14 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { InjectRepository } from '@nestjs/typeorm';
+import { traceDeprecation } from 'process';
+import { Collection } from 'src/collections/entities/collection.entity';
 import { logger } from 'src/common/logger';
+import { Content } from 'src/contents/entities/content.entity';
+import { Between, Repository } from 'typeorm';
 
 @Injectable() // Only support SINGLETON scope
 export class TaskService {
-  @Cron('* * * * * *', {
+  constructor(
+    @InjectRepository(Content)
+    private readonly contents: Repository<Content>,
+  ) {}
+
+  @Cron('10 * * * * *', {
     name: 'testjob',
     timeZone: 'Asia/Seoul',
   })
   cronJob() {
     logger.debug('Cron job is running');
+  }
+
+  // 매일 아침 8시에 작업 실행
+  @Cron('* * * * * *', {
+    name: "check article's deadline",
+    timeZone: 'Asia/Seoul',
+  })
+  async checkDeadline() {
+    // 만료될 콘텐츠를 찾기 위한 날짜 값 생성
+    const date = new Date();
+    const year = date.getFullYear(); // 년
+    const month = date.getMonth(); // 월
+    const day = date.getDate(); // 일
+
+    const utcToday = new Date(
+      new Date(year, month, day, 0, 0, 0, 0).toUTCString(),
+    );
+
+    // 만료될 콘텐츠를 찾아서 알림
+    logger.info("Check article's deadline");
+    const contents = await this.contents.find({
+      where: {
+        deadline: utcToday,
+      },
+    });
+    console.log(contents);
   }
 }

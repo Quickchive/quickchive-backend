166047841b9869ed2fd702888846ffccd2ae23b5
hou27 <ataj125@gmail.com>
2022-09-30 16:56:05 +0900

    feat: 문서 요약을 위한 크롤링 및 후처리 구현 - #102
    문서 요약을 위한 본문 크롤링 후 (p 태그 탐색)
    1900자씩 잘라 요약 후 이어붙여 반환

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export { default as getOrCreateCategory } from './getOrCreateCategory';
 export { default as init } from './init';
 export { default as getLinkInfo } from './getLinkInfo';
 export { default as getKoreaTime } from './getKoreaTime';
+export { default as getDocument } from './getDocument';

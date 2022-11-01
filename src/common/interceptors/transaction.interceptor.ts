4a85c86ec0ac048839b564f2cf707e302dd91778
hou27 <ataj125@gmail.com>
2022-11-01 15:38:29 +0900

    feat: Implement transaction interceptor - #120
    공통 관심사 분리를 위해 transaction을 관리하는
    interceptor 구현

---
+++ src/common/interceptors/transaction.interceptor.ts
@@ -0,0 +1,55 @@
+// @Injectable()
+// export class TransactionInterceptor implements NestInterceptor {
+
+import {
+  CallHandler,
+  ExecutionContext,
+  HttpException,
+  Injectable,
+  InternalServerErrorException,
+  NestInterceptor,
+} from '@nestjs/common';
+import { catchError, Observable, tap } from 'rxjs';
+import { DataSource, QueryRunner } from 'typeorm';
+
+@Injectable()
+export class TransactionInterceptor implements NestInterceptor {
+  constructor(private readonly dataSource: DataSource) {}
+  async intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Promise<Observable<any>> {
+    const req = context.switchToHttp().getRequest();
+    const queryRunner = await this.dbInit();
+
+    req.queryRunnerManager = queryRunner.manager;
+
+    return next.handle().pipe(
+      catchError(async (e) => {
+        await queryRunner.rollbackTransaction();
+        await queryRunner.release();
+
+        if (e instanceof HttpException) {
+          throw new HttpException(
+            e.message,
+            e.getStatus() ? e.getStatus() : 500,
+          );
+        } else {
+          throw new InternalServerErrorException();
+        }
+      }),
+      tap(async () => {
+        await queryRunner.commitTransaction();
+        await queryRunner.release();
+      }),
+    );
+  }
+
+  async dbInit(): Promise<QueryRunner> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    return queryRunner;
+  }
+}

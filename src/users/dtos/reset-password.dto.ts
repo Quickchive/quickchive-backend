2b80b677b8704f8b517f7848e8d96e4988ad7d88
hou27 <ataj125@gmail.com>
2022-12-29 00:20:58 +0900

    feat: migration 파일에 방해되는 불필요한 entity 삭제 - #138
    verification entity 제거

--- src/users/dtos/reset-password.dto.ts
+++ src/users/dtos/reset-password.dto.ts
@@ -1,10 +1,12 @@
 import { ApiProperty, IntersectionType, PickType } from '@nestjs/swagger';
 import { CoreOutput } from 'src/common/dtos/output.dto';
 import { User } from '../entities/user.entity';
-import { Verification } from '../entities/verification.entity';
 
 class passwordForResetPassword extends PickType(User, ['password']) {}
-class codeForResetPassword extends PickType(Verification, ['code']) {}
+class codeForResetPassword {
+  @ApiProperty({ description: 'Authentication code' })
+  code: string;
+}
 
 export class ResetPasswordInput extends IntersectionType(
   passwordForResetPassword,
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,13 @@
 import { CacheModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { Verification } from './entities/verification.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Verification]),
+    TypeOrmModule.forFeature([User]),
     CacheModule.register({
       store: redisStore,
       host: process.env.REDIS_HOST,

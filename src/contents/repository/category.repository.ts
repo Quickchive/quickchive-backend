650f5cade4bce2ad272cb5b4301c1b1b9a0e5a24
hou27 <ataj125@gmail.com>
2022-12-29 18:56:06 +0900

    feat: category coustom method 수정 - #137
    변경된 table 간 관계에 따라 custom method도 수정

--- src/contents/repository/category.repository.ts
+++ src/contents/repository/category.repository.ts
@@ -1,37 +1,68 @@
 import { EntityManager, Repository } from 'typeorm';
 import { Category } from '../entities/category.entity';
+import { User } from '../../users/entities/user.entity';
+import { ConflictException } from '@nestjs/common';
+import { categoryNameAndSlug } from '../dtos/category.dto';
 
 export interface CategoryRepository extends Repository<Category> {
   this: Repository<Category>;
 
-  getOrCreate(
+  checkAndCreate(
+    user: User,
     name: string,
     queryRunnerManager: EntityManager,
-  ): Promise<Category>;
+  ): Promise<void>;
+
+  generateNameAndSlug(name: string): categoryNameAndSlug;
 }
 
-type CustomCategoryRepository = Pick<CategoryRepository, 'getOrCreate'>;
+type CustomCategoryRepository = Pick<
+  CategoryRepository,
+  'checkAndCreate' | 'generateNameAndSlug'
+>;
 
 export const customCategoryRepositoryMethods: CustomCategoryRepository = {
-  async getOrCreate(
+  /**
+   * Check if category exists and
+   * create or throw duplicate error
+   */
+  async checkAndCreate(
+    user: User,
     name: string,
     queryRunnerManager: EntityManager,
-  ): Promise<Category> {
+  ): Promise<void> {
     const categoryName = name.trim().toLowerCase();
     const categorySlug = categoryName.replace(/ /g, '-');
-    let category = await queryRunnerManager.findOneBy(Category, {
-      slug: categorySlug,
-    });
+    // let category = await queryRunnerManager.findOneBy(Category, {
+    //   slug: categorySlug,
+    // });
 
-    if (!category) {
-      category = await queryRunnerManager.save(
+    // check if category exists in user's categories
+    const category = user.categories.find(
+      (category) => category.slug === categorySlug,
+    );
+
+    // if category doesn't exist, create it
+    if (category) {
+      throw new ConflictException('Category already exists');
+    } else {
+      const category = await queryRunnerManager.save(
         queryRunnerManager.create(Category, {
           slug: categorySlug,
           name: categoryName,
+          user,
         }),
       );
-    }
 
-    return category;
+      user.categories.push(category);
+      await queryRunnerManager.save(user);
+    }
+  },
+
+  generateNameAndSlug(name: string): categoryNameAndSlug {
+    const categoryName = name.trim().toLowerCase();
+    const categorySlug = categoryName.replace(/ /g, '-');
+
+    return { categoryName, categorySlug };
   },
 };

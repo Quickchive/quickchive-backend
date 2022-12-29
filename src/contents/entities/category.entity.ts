b302b7baa5e7222ebde65660b8e0215f87489595
hou27 <ataj125@gmail.com>
2022-12-29 19:54:56 +0900

    fix: category entity에 실수로 제거한 column 데코레이터 추가 - #137
    name과 slug에 colum 데코레이터 추가

--- src/contents/entities/category.entity.ts
+++ src/contents/entities/category.entity.ts
@@ -9,11 +9,13 @@ import { User } from '../../users/entities/user.entity';
 @Entity()
 export class Category extends CoreEntity {
   @ApiProperty({ description: 'Category Name' })
+  @Column()
   @IsString()
   @Length(2)
   name: string;
 
   @ApiProperty({ description: 'Category Slug' })
+  @Column()
   @IsString()
   slug: string;
 

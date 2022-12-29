fbcba44c8abd27e37bc7fc7bf22dac3f6fed5fa7
hou27 <ataj125@gmail.com>
2022-12-29 18:58:03 +0900

    feat: 불필요한 custom method 삭제
    checkAndCreate method 삭제

--- src/contents/repository/category.repository.ts
+++ src/contents/repository/category.repository.ts
@@ -1,64 +1,16 @@
-import { EntityManager, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Category } from '../entities/category.entity';
-import { User } from '../../users/entities/user.entity';
-import { ConflictException } from '@nestjs/common';
 import { categoryNameAndSlug } from '../dtos/category.dto';
 
 export interface CategoryRepository extends Repository<Category> {
   this: Repository<Category>;
 
-  checkAndCreate(
-    user: User,
-    name: string,
-    queryRunnerManager: EntityManager,
-  ): Promise<void>;
-
   generateNameAndSlug(name: string): categoryNameAndSlug;
 }
 
-type CustomCategoryRepository = Pick<
-  CategoryRepository,
-  'checkAndCreate' | 'generateNameAndSlug'
->;
+type CustomCategoryRepository = Pick<CategoryRepository, 'generateNameAndSlug'>;
 
 export const customCategoryRepositoryMethods: CustomCategoryRepository = {
-  /**
-   * Check if category exists and
-   * create or throw duplicate error
-   */
-  async checkAndCreate(
-    user: User,
-    name: string,
-    queryRunnerManager: EntityManager,
-  ): Promise<void> {
-    const categoryName = name.trim().toLowerCase();
-    const categorySlug = categoryName.replace(/ /g, '-');
-    // let category = await queryRunnerManager.findOneBy(Category, {
-    //   slug: categorySlug,
-    // });
-
-    // check if category exists in user's categories
-    const category = user.categories.find(
-      (category) => category.slug === categorySlug,
-    );
-
-    // if category doesn't exist, create it
-    if (category) {
-      throw new ConflictException('Category already exists');
-    } else {
-      const category = await queryRunnerManager.save(
-        queryRunnerManager.create(Category, {
-          slug: categorySlug,
-          name: categoryName,
-          user,
-        }),
-      );
-
-      user.categories.push(category);
-      await queryRunnerManager.save(user);
-    }
-  },
-
   generateNameAndSlug(name: string): categoryNameAndSlug {
     const categoryName = name.trim().toLowerCase();
     const categorySlug = categoryName.replace(/ /g, '-');

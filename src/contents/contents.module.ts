a0924ac81d02f13c8a02ed9aaf6da11f806ba406
hou27 <ataj125@gmail.com>
2023-01-11 01:04:22 +0900

    feat: contents module에서 redis 사용 준비 - #145

--- src/contents/contents.module.ts
+++ src/contents/contents.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import {
   getDataSourceToken,
   getRepositoryToken,
@@ -15,9 +15,20 @@ import { CategoryService, ContentsService } from './contents.service';
 import { Category } from './entities/category.entity';
 import { Content } from './entities/content.entity';
 import { customCategoryRepositoryMethods } from './repository/category.repository';
+import * as redisStore from 'cache-manager-redis-store';
+
+// 카테고리에 저장된 콘텐츠 카운트 캐시에 2달간 저장을 위한 시간 값
+export const categoryCountExpirationInCache = 60 * 60 * 24 * 60;
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Content, Category])],
+  imports: [
+    TypeOrmModule.forFeature([User, Content, Category]),
+    CacheModule.register({
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+    }),
+  ],
   controllers: [ContentsController, CategoryController, TestController],
   providers: [
     ContentsService,

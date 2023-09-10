3f6c7353a5aad9a1aeeb3d721b48e5b0bfda2ba7
hou27 <ataj125@gmail.com>
2023-09-10 17:34:14 +0900

    feat: 본문 크롤링을 위한 함수 구현 - #183
    p 태그를 긁어오는 것으로 구현해둠.
    (현재 불필요한 글까지 함께 가져와지지만 하나하나 예외처리말고는 방법 없는 상황)

--- src/contents/util/content.util.ts
+++ src/contents/util/content.util.ts
@@ -60,4 +60,39 @@ export class ContentUtil {
       siteName,
     };
   }
+
+  async getLinkContent(link: string) {
+    let content: string | undefined = '';
+
+    if (!link.match(/^(http|https):\/\//)) {
+      link = `http://${link}`;
+    }
+
+    await axios
+      .get(link)
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new BadRequestException('잘못된 링크입니다.');
+        } else {
+          const data = res.data;
+          if (typeof data === 'string') {
+            const $ = cheerio.load(data);
+            content = '';
+            $('p').each((i, elem) => {
+              // 모든 p 태그에 대해 작업을 수행합니다.
+              content += $(elem).text() + '\n'; // 각 p 태그의 텍스트를 가져와서 누적합니다.
+            });
+          }
+        }
+      })
+      .catch((e) => {
+        // Control unreachable link
+        // if(e.message === 'Request failed with status code 403') {
+        // 403 에러가 발생하는 링크는 크롤링이 불가능한 링크이다.
+        // }
+        content = '';
+      });
+
+    return content;
+  }
 }

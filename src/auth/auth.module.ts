e105d5e2e99136b344fcaa0fdf4b92ddda5eb3be
stae1102 <stae1102@gmail.com>
2024-03-29 23:58:00 +0900

    feat: replace cache manager to redis service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,10 @@
-import { CacheModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { OAuthController } from './oauth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt/jwt.strategy';
-import * as redisStore from 'cache-manager-redis-store';
 import { GoogleStrategy } from './passport/google/google.strategy';
 import { customJwtService } from './jwt/jwt.service';
 import { TWOHOUR } from './jwt/jwt.payload';
@@ -14,6 +13,7 @@ import { OAuthUtil } from './util/oauth.util';
 import { ContentsModule } from '../contents/contents.module';
 import { OAuthService } from './oauth.service';
 import { CategoryModule } from '../categories/category.module';
+import { RedisModule } from '../infra/redis/redis.module';
 
 const accessTokenExpiration = TWOHOUR;
 export const refreshTokenExpirationInCache = 60 * 60 * 24 * 365; // 1 year
@@ -31,12 +31,8 @@ export const verifyEmailExpiration = 60 * 5;
     }),
     UsersModule,
     ContentsModule,
-    CacheModule.register({
-      store: redisStore,
-      host: process.env.REDIS_HOST,
-      port: process.env.REDIS_PORT,
-    }),
     CategoryModule,
+    RedisModule,
   ],
   controllers: [AuthController, OAuthController],
   providers: [
